89293cff2c10fc7ce770b02bdbc7e6c617f615eb
Paweł Więckowski <pawel.wieckowski@schange.com>
2020-10-11 18:44:38 +0200

    Rename POC app to ReactNativeRCU

--- android/app/src/main/java/com/reactnativercu/MainActivity.java
+++ android/app/src/main/java/com/reactnativercu/MainActivity.java
@@ -1,4 +1,4 @@
-package com.testapp5;
+package com.reactnativercu;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "TestApp5";
+    return "ReactNativeRCU";
   }
 }
--- ios/ReactNativeRCUTests/ReactNativeRCUTests.m
+++ ios/ReactNativeRCUTests/ReactNativeRCUTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TestApp5Tests : XCTestCase
+@interface ReactNativeRCUTests : XCTestCase
 
 @end
 
-@implementation TestApp5Tests
+@implementation ReactNativeRCUTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
